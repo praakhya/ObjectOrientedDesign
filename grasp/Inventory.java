076ee1a1701014e02d760e762c859253c71fae33
praakhya <67508200+praakhya@users.noreply.github.com>
2024-04-24 15:42:13 +0530

    GRASP - information expert principle

--- grasp/Inventory.java
+++ grasp/Inventory.java
@@ -1,15 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Inventory { //This class has responsibility of a creator
+public class Inventory { //This class is the information expert
     Map<Item, Integer> items; //It contains the items
     Inventory() {
         items = new HashMap<>();
     }
     void addItem(String description, double price, String itemID, int quantity) {
         Item newItem = new Item(
-                description, price, itemID //It initialises data for each item
+                description, price, itemID //It initialises data for each item (creator)
         );
         items.put(newItem, quantity);
+    }
+    int getNumberOfItems() {
+        //The information expert has information about all items and can return count of items
+        int sum=0;
+        for (Map.Entry<Item, Integer> entry : items.entrySet()) {
+            sum+=entry.getValue();
+        }
+        return sum;
     }
 }
